pinheadmz/bitcoin

Allow http workers to send data optimistically as an optimization

src/httpserver.cpp
@@ -1050,9 +1050,11 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
     const std::string headers{res.StringifyHeaders()};
     const auto headers_bytes{std::as_bytes(std::span(headers.begin(), headers.end()))};
 
+    bool send_buffer_was_empty{false};
     // Fill the send buffer with the complete serialized response headers + body
     {
         LOCK(m_client->m_send_mutex);
+        send_buffer_was_empty = m_client->m_send_buffer.empty();
         m_client->m_send_buffer.insert(m_client->m_send_buffer.end(), headers_bytes.begin(), headers_bytes.end());
 
         // We've been using std::span up until now but it is finally time to copy
@@ -1061,10 +1063,6 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
         m_client->m_send_buffer.insert(m_client->m_send_buffer.end(), reply_body.begin(), reply_body.end());
     }
 
-    // Inform Sockman I/O there is data that is ready to be sent to this client
-    // in the next loop iteration.
-    m_client->m_send_ready = true;
-
     LogDebug(
         BCLog::HTTP,
         "HTTPResponse (status code: %d size: %lld) added to send buffer for client %s (id=%lld)\n",
@@ -1072,6 +1070,18 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
         headers_bytes.size() + reply_body.size(),
         m_client->m_origin,
         m_client->m_node_id);
+
+    // If the send buffer was empty before we wrote this reply, we can try an
+    // optimistic send akin to CConnman::PushMessage() in which we
+    // push the data directly out the socket to client right now, instead
+    // of waiting for the next iteration of the Sockman I/O loop.
+    if (send_buffer_was_empty) {
+        m_client->SendBytesFromBuffer();
+    } else {
+        // Inform Sockman I/O there is data that is ready to be sent to this client
+        // in the next loop iteration.
+        m_client->m_send_ready = true;
+    }
 }
 
 bool HTTPClient::ReadRequest(std::unique_ptr<HTTPRequest>& req)
@@ -1137,6 +1147,9 @@ bool HTTPClient::SendBytesFromBuffer()
                 m_disconnect = true;
                 return false;
             }
+        } else {
+            m_send_ready = true;
+            m_prevent_disconnect = true;
         }
     }
 

tavecchiacoin/tav

src/httpserver.cpp
@@ -951,9 +951,11 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
     const std::string headers{res.StringifyHeaders()};
     const auto headers_bytes{std::as_bytes(std::span(headers.begin(), headers.end()))};
 
+    bool send_buffer_was_empty{false};
     // Fill the send buffer with the complete serialized response headers + body
     {
         LOCK(m_client->m_send_mutex);
+        send_buffer_was_empty = m_client->m_send_buffer.empty();
         m_client->m_send_buffer.insert(m_client->m_send_buffer.end(), headers_bytes.begin(), headers_bytes.end());
 
         // We've been using std::span up until now but it is finally time to copy
@@ -962,10 +964,6 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
         m_client->m_send_buffer.insert(m_client->m_send_buffer.end(), reply_body.begin(), reply_body.end());
     }
 
-    // Inform HTTPServer I/O loop that there is data that is ready to be sent to
-    // this client in the next loop iteration.
-    m_client->m_send_ready = true;
-
     LogDebug(
         BCLog::HTTP,
         "HTTPResponse (status code: %d size: %lld) added to send buffer for client %s (id=%lld)",
@@ -973,6 +971,18 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
         headers_bytes.size() + reply_body.size(),
         m_client->m_origin,
         m_client->m_id);
+
+    // If the send buffer was empty before we wrote this reply, we can try an
+    // optimistic send akin to CConnman::PushMessage() in which we
+    // push the data directly out the socket to client right now, instead
+    // of waiting for the next iteration of the I/O loop.
+    if (send_buffer_was_empty) {
+        m_client->MaybeSendBytesFromBuffer();
+    } else {
+        // Inform HTTPServer I/O that data is ready to be sent to this client
+        // in the next loop iteration.
+        m_client->m_send_ready = true;
+    }
 }
 
 util::Result<void> HTTPServer::BindAndStartListening(const CService& to)
@@ -1425,6 +1435,10 @@ bool HTTPClient::MaybeSendBytesFromBuffer()
                 // Do not attempt to read from this client.
                 return false;
             }
+        } else {
+            // The send buffer isn't flushed yet, try to push more on the next loop.
+            m_send_ready = true;
+            m_prevent_disconnect = true;
         }
     }
 