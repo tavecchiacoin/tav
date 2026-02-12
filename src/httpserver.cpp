bitcoin/bitcoin

Add helper methods to HTTPRequest to match original API
These methods are called by http_request_cb() and are present in the
original http_libevent::HTTPRequest.

src/httpserver.cpp
@@ -9,6 +9,7 @@
 #include <chainparamsbase.h>
 #include <common/args.h>
 #include <common/messages.h>
+#include <common/url.h>
 #include <compat/compat.h>
 #include <logging.h>
 #include <netbase.h>
@@ -1058,6 +1059,60 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
 
     // Signal to the I/O loop that we are ready to handle the next request.
     m_client->m_req_busy = false;
+}
+
+CService HTTPRequest::GetPeer() const
+{
+    return m_client->m_addr;
+}
+
+std::optional<std::string> HTTPRequest::GetQueryParameter(const std::string& key) const
+{
+    return GetQueryParameterFromUri(GetURI(), key);
+}
+
+// See libevent http.c evhttp_parse_query_impl()
+// and https://www.rfc-editor.org/rfc/rfc3986#section-3.4
+std::optional<std::string> GetQueryParameterFromUri(const std::string& uri, const std::string& key)
+{
+    // Handle %XX encoding
+    std::string decoded_uri{UrlDecode(uri)};
+
+    // find query in URI
+    size_t start = decoded_uri.find('?');
+    if (start == std::string::npos) return std::nullopt;
+    size_t end = decoded_uri.find('#', start);
+    if (end == std::string::npos) {
+        end = decoded_uri.length();
+    }
+    const std::string_view query{decoded_uri.data() + start + 1, end - start - 1};
+    // find requested parameter in query
+    const std::vector<std::string_view> params{Split<std::string_view>(query, "&")};
+    for (const std::string_view& param : params) {
+        size_t delim = param.find('=');
+        if (key == param.substr(0, delim)) {
+            if (delim == std::string::npos) {
+                return "";
+            } else {
+                return std::string(param.substr(delim + 1));
+            }
+        }
+    }
+    return std::nullopt;
+}
+
+std::pair<bool, std::string> HTTPRequest::GetHeader(const std::string& hdr) const
+{
+    std::optional<std::string> found{m_headers.Find(hdr)};
+    if (found.has_value()) {
+        return std::make_pair(true, found.value());
+    } else
+        return std::make_pair(false, "");
+}
+
+void HTTPRequest::WriteHeader(const std::string& hdr, const std::string& value)
+{
+    m_response_headers.Write(hdr, value);
 }
 
 util::Result<void> HTTPServer::BindAndStartListening(const CService& to)

tavecchiacoin/tav

src/httpserver.cpp
@@ -9,6 +9,7 @@
 #include <chainparamsbase.h>
 #include <common/args.h>
 #include <common/messages.h>
+#include <common/url.h>
 #include <compat/compat.h>
 #include <logging.h>
 #include <netbase.h>
@@ -994,6 +995,60 @@ void HTTPRequest::WriteReply(HTTPStatusCode status, std::span<const std::byte> r
 
     // Signal to the I/O loop that we are ready to handle the next request.
     m_client->m_req_busy = false;
+}
+
+CService HTTPRequest::GetPeer() const
+{
+    return m_client->m_addr;
+}
+
+std::optional<std::string> HTTPRequest::GetQueryParameter(const std::string& key) const
+{
+    return GetQueryParameterFromUri(GetURI(), key);
+}
+
+// See libevent http.c evhttp_parse_query_impl()
+// and https://www.rfc-editor.org/rfc/rfc3986#section-3.4
+std::optional<std::string> GetQueryParameterFromUri(const std::string& uri, const std::string& key)
+{
+    // Handle %XX encoding
+    std::string decoded_uri{UrlDecode(uri)};
+
+    // find query in URI
+    size_t start = decoded_uri.find('?');
+    if (start == std::string::npos) return std::nullopt;
+    size_t end = decoded_uri.find('#', start);
+    if (end == std::string::npos) {
+        end = decoded_uri.length();
+    }
+    const std::string_view query{decoded_uri.data() + start + 1, end - start - 1};
+    // find requested parameter in query
+    const std::vector<std::string_view> params{Split<std::string_view>(query, "&")};
+    for (const std::string_view& param : params) {
+        size_t delim = param.find('=');
+        if (key == param.substr(0, delim)) {
+            if (delim == std::string::npos) {
+                return "";
+            } else {
+                return std::string(param.substr(delim + 1));
+            }
+        }
+    }
+    return std::nullopt;
+}
+
+std::pair<bool, std::string> HTTPRequest::GetHeader(const std::string& hdr) const
+{
+    std::optional<std::string> found{m_headers.FindFirst(hdr)};
+    if (found.has_value()) {
+        return std::make_pair(true, found.value());
+    } else
+        return std::make_pair(false, "");
+}
+
+void HTTPRequest::WriteHeader(const std::string& hdr, const std::string& value)
+{
+    m_response_headers.Write(hdr, value);
 }
 
 util::Result<void> HTTPServer::BindAndStartListening(const CService& to)