eb1e404aacc7c82baf135648e44e7208f1d484f4
fanquake <fanquake@gmail.com>
2026-02-18 10:43:02 +0000

    leveldb: remove unused db/dumpfile.cc & util/histogram.cc

--- cmake/leveldb.cmake
+++ cmake/leveldb.cmake
@@ -15,7 +15,6 @@ add_library(leveldb STATIC EXCLUDE_FROM_ALL
   ${PROJECT_SOURCE_DIR}/src/leveldb/db/db_impl.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/db/db_iter.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/db/dbformat.cc
-  ${PROJECT_SOURCE_DIR}/src/leveldb/db/dumpfile.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/db/filename.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/db/log_reader.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/db/log_writer.cc
@@ -45,7 +44,6 @@ add_library(leveldb STATIC EXCLUDE_FROM_ALL
   $<$<BOOL:${WIN32}>:${PROJECT_SOURCE_DIR}/src/leveldb/util/env_windows.cc>
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/filter_policy.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/hash.cc
-  ${PROJECT_SOURCE_DIR}/src/leveldb/util/histogram.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/logging.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/options.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/status.cc
