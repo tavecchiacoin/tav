b217614c6d2dc878e305ff6dc88c8baac38c501d
TavecchiaCoin Bank Of Me <gt@tavecchiacoin.com>
2026-03-10 17:48:15 +0100

    Update chainparams.cpp

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -111,10 +111,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xf9;
-        pchMessageStart[1] = 0xbe;
-        pchMessageStart[2] = 0xb4;
-        pchMessageStart[3] = 0xd9;
+      pchMessageStart[0] = 0x54;
+      pchMessageStart[1] = 0x41;
+      pchMessageStart[2] = 0x56;
+      pchMessageStart[3] = 0x31;
         nDefaultPort = 8333;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 540;
